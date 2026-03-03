d4cc8de090658204db09332eaaf8355eeba5b6b8
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-03-03 02:25:13 -0300

    Created ReadBook.ts and added it in book schemas

---
+++ src/models/entities/ReadBook.ts
@@ -0,0 +1,22 @@
+import { Book } from "./Book.ts";
+import { ReadBookSchema, type ReadBookProps } from "../schemas/book.schema.ts";
+
+export class ReadBook extends Book {
+    private rating: number;
+    private review: string | null;
+
+    private constructor (props: ReadBookProps) {
+        const { rating, review, ...baseProps } = props; 
+        
+        super(baseProps);
+
+        this.rating = rating;
+        this.review = review;
+    }
+
+    static create(data: unknown) {
+        const validatedProps = ReadBookSchema.parse(data);
+
+        return new ReadBook(validatedProps);
+    }
+}
--- src/models/schemas/book.schema.ts
+++ src/models/schemas/book.schema.ts
@@ -12,5 +12,11 @@ export const DesiredBookSchema = BookSchema.extend({
     link: z.url()
 }) 
 
+export const ReadBookSchema = BookSchema.extend({
+    rating: z.number().min(0).max(5),
+    review: z.string().nullable().default(null)
+})
+
 export type BookProps = z.infer<typeof BookSchema>;
 export type DesiredBookProps = z.infer<typeof DesiredBookSchema>;
+export type ReadBookProps = z.infer<typeof ReadBookSchema>
