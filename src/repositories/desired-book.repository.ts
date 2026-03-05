4b45fe4647138b779cebe337c2b0cfd84f910b7a
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-03-04 23:28:08 -0300

    create desired-book.repository.ts

--- src/repositories/desired-book.repository.ts
+++ src/repositories/desired-book.repository.ts
@@ -21,21 +21,64 @@ export interface IDesiredBookRepository {
 export class SQLDesiredBookRepository implements IDesiredBookRepository {
   async findById(id: string): Promise<DesiredBook | null> {
     const [rows]: [DesiredBookDatabaseRow[], FieldPacket[]] =
-      await pool.execute('SELECT * FROM desired_books WHERE id = ?', [id]);
+      await pool.execute(
+        `SELECT BIN_TO_UUID(b.id) as id, b.title, b.cover, b.author, 
+              BIN_TO_UUID(b.userId) as userId, b.description, d.link 
+       FROM books b
+       JOIN desired_books d ON b.id = d.id
+       WHERE b.id = UUID_TO_BIN(?)`,
+        [id]
+      );
 
     const book = rows[0];
+    return book ? DesiredBook.create(book) : null;
+  }
 
-    if (!book) {
-      return null;
+  async findAllByUserId(userId: string): Promise<DesiredBook[] | null> {
+    const [rows]: [DesiredBookDatabaseRow[], FieldPacket[]] =
+      await pool.execute(
+        `SELECT BIN_TO_UUID(b.id) as id, b.title, b.cover, b.author, 
+              BIN_TO_UUID(b.userId) as userId, b.description, d.link 
+       FROM books b
+       JOIN desired_books d ON b.id = d.id
+       WHERE b.userId = UUID_TO_BIN(?)`,
+        [userId]
+      );
+
+    if (rows.length === 0) return null;
+    return rows.map((row) => DesiredBook.create(row));
+  }
+
+  async save(book: DesiredBook) {
+    const connection = await pool.getConnection();
+    try {
+      await connection.beginTransaction();
+
+      await connection.execute(
+        `INSERT INTO books (id, title, cover, author, userId, description) 
+         VALUES (UUID_TO_BIN(?), ?, ?, ?, UUID_TO_BIN(?), ?)`,
+        [
+          book.id!,
+          book.title,
+          book.cover,
+          book.author,
+          book.userId,
+          book.description,
+        ]
+      );
+
+      await connection.execute(
+        `INSERT INTO desired_books (id, link) VALUES (UUID_TO_BIN(?), ?)`,
+        [book.id!, book.link]
+      );
+
+      await connection.commit();
+      return [null, true];
+    } catch (e) {
+      await connection.rollback();
+      return [e as Error, null];
+    } finally {
+      connection.release();
     }
-
-    return DesiredBook.create({
-      id: z.uuid().optional(),
-      title: z.string().min(1).max(150),
-      fcover: z.url(),
-      author: z.string().min(1),
-      userId: z.uuid(),
-      description: z.string().max(3000).nullable().default(null),
-    });
   }
 }
